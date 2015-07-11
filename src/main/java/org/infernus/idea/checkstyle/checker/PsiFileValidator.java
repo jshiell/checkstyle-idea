9d0dc7f6236e8edf9219ca0b89c37675d8482635
James Shiell <james@infernus.org>
2015-07-11 15:21:16 +0100

    Java source files that are marked as generated are no longer scanned (#172).

--- src/main/java/org/infernus/idea/checkstyle/checker/PsiFileValidator.java
+++ src/main/java/org/infernus/idea/checkstyle/checker/PsiFileValidator.java
@@ -1,15 +1,10 @@
 package org.infernus.idea.checkstyle.checker;
 
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.roots.JavaProjectRootsUtil;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.psi.PsiFile;
-import org.infernus.idea.checkstyle.util.Paths;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public final class PsiFileValidator {
 
@@ -21,36 +16,11 @@ public final class PsiFileValidator {
                 && psiFile.isValid()
                 && psiFile.isPhysical()
                 && isInSource(psiFile)
-                && isNotInOutputPaths(psiFile);
+                && !isGenerated(psiFile);
     }
 
-    private static boolean isNotInOutputPaths(@NotNull final PsiFile psiFile) {
-        final String fileUrl = urlOf(psiFile);
-        if (fileUrl == null) {
-            return true;
-        }
-
-        for (Module module : ModuleManager.getInstance(psiFile.getProject()).getModules()) {
-            for (URL outputPath : Paths.compilerOutputPathsFor(module)) {
-                if (fileUrl.startsWith(outputPath.toString())) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private static String urlOf(@NotNull final PsiFile psiFile) {
-        if (psiFile.getVirtualFile() != null) {
-            try {
-                // getUrl() formats a file root as file:///, URL formats as file:/
-                return new URL(psiFile.getVirtualFile().getUrl()).toString();
-            } catch (MalformedURLException ignored) {
-            }
-        }
-
-        return null;
+    private static boolean isGenerated(final PsiFile psiFile) {
+        return JavaProjectRootsUtil.isInGeneratedCode(psiFile.getVirtualFile(), psiFile.getProject());
     }
 
     private static boolean isInSource(@NotNull final PsiFile psiFile) {
