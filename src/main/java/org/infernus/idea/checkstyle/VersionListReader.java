4e608b97d653a9ed9e224917eabc6e355d45e032
Thomas Jensen <tsjensen@users.noreply.github.com>
2017-07-29 13:57:23 +0200

    Add ability to replace configured Checkstyle versions with compatible ones

--- src/main/java/org/infernus/idea/checkstyle/VersionListReader.java
+++ src/main/java/org/infernus/idea/checkstyle/VersionListReader.java
@@ -1,14 +1,21 @@
 package org.infernus.idea.checkstyle;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import com.intellij.openapi.util.Pair;
 import org.apache.commons.io.IOUtils;
 import org.infernus.idea.checkstyle.exception.CheckStylePluginException;
 import org.infernus.idea.checkstyle.util.Strings;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
-import java.util.function.Function;
 
 
 /**
@@ -21,8 +28,10 @@ class VersionListReader
 
     private static final String PROP_NAME_JAVA7 = "checkstyle.versions.java7";
     private static final String PROP_NAME_JAVA8 = "checkstyle.versions.java8";
+    private static final String PROP_VERSION_MAP = "checkstyle.versions.map";
 
     private final SortedSet<String> supportedVersions;
+    private final SortedMap<String, String> replacementMap;
 
 
     VersionListReader() {
@@ -30,27 +39,9 @@ class VersionListReader
     }
 
     VersionListReader(@NotNull final String pPropertyFile) {
-        supportedVersions = readSupportedVersions(pPropertyFile);
-    }
-
-
-    /**
-     * Read the supported Checkstyle versions from the config properties file.
-     *
-     * @param pPropertyFile file name of the property file to be passed to {@link ClassLoader#getResourceAsStream}
-     * @return the supported versions which match the Java level of the current JVM
-     */
-    @NotNull
-    private SortedSet<String> readSupportedVersions(@NotNull final String pPropertyFile) {
         final Properties props = readProperties(pPropertyFile);
-        final String javaVersion = Runtime.class.getPackage().getSpecificationVersion();
-
-        final SortedSet<String> theVersions = new TreeSet<>(new VersionComparator());
-        theVersions.addAll(readVersions(pPropertyFile, props, PROP_NAME_JAVA7));
-        if (!javaVersion.startsWith("1.7")) {
-            theVersions.addAll(readVersions(pPropertyFile, props, PROP_NAME_JAVA8));
-        }
-        return Collections.unmodifiableSortedSet(theVersions);
+        supportedVersions = readSupportedVersions(pPropertyFile, props);
+        replacementMap = readVersionMap(pPropertyFile, props, supportedVersions);
     }
 
 
@@ -81,6 +72,27 @@ class VersionListReader
     }
 
 
+    /**
+     * Read the supported Checkstyle versions from the config properties file.
+     *
+     * @param pPropertyFile file name of the property file to be passed to {@link ClassLoader#getResourceAsStream}
+     * @param props the properties read from the property file
+     * @return the supported versions which match the Java level of the current JVM
+     */
+    @NotNull
+    private SortedSet<String> readSupportedVersions(@NotNull final String pPropertyFile,
+                                                    @NotNull final Properties props) {
+        final String javaVersion = Runtime.class.getPackage().getSpecificationVersion();
+
+        final SortedSet<String> theVersions = new TreeSet<>(new VersionComparator());
+        theVersions.addAll(readVersions(pPropertyFile, props, PROP_NAME_JAVA7));
+        if (!javaVersion.startsWith("1.7")) {
+            theVersions.addAll(readVersions(pPropertyFile, props, PROP_NAME_JAVA8));
+        }
+        return Collections.unmodifiableSortedSet(theVersions);
+    }
+
+
     @NotNull
     private Set<String> readVersions(@NotNull final String pPropertyFile, @NotNull final Properties props,
                                      @NotNull final String propertyName) {
@@ -107,6 +119,63 @@ class VersionListReader
 
 
     @NotNull
+    private SortedMap<String, String> readVersionMap(@NotNull final String pPropertyFile,
+                                                     @NotNull final Properties props,
+                                                     @NotNull final SortedSet<String> pSupportedVersions) {
+        final String propertyValue = props.getProperty(PROP_VERSION_MAP);
+        if (Strings.isBlank(propertyValue)) {
+            throw new CheckStylePluginException("Internal error: Property '" + PROP_VERSION_MAP + "' missing from "
+                    + "configuration file '" + pPropertyFile + "'");
+        }
+
+        final String[] mappings = propertyValue.trim().split("\\s*,\\s*");
+        final SortedMap<String, String> result = new TreeMap<>(new VersionComparator());
+        for (final String mapping : mappings) {
+            if (!mapping.isEmpty()) {
+                final Pair<String, String> validMapping = readValidMapping(pPropertyFile, mapping, pSupportedVersions);
+                if (result.containsKey(validMapping.getFirst())) {
+                    throw new CheckStylePluginException("Internal error: Property '" + PROP_VERSION_MAP + "' "
+                            + "contains duplicate mapping \"" + mapping + "\" in configuration file '" + pPropertyFile
+                            + "'");
+                }
+                result.put(validMapping.getFirst(), validMapping.getSecond());
+            }
+        }
+        return Collections.unmodifiableSortedMap(result);
+    }
+
+
+    private Pair<String, String> readValidMapping(@NotNull final String pPropertyFile, @NotNull final String
+            pMapping, @NotNull final SortedSet<String> pSupportedVersions) {
+
+        final String[] kv = pMapping.split("\\s*->\\s*");
+        if (kv.length != 2) {
+            throw new CheckStylePluginException("Internal error: Property '" + PROP_VERSION_MAP + "' contains "
+                    + "invalid mapping '" + pMapping + "' in configuration file '" + pPropertyFile + "'");
+        }
+
+        final String unsupportedVersion = kv[0];
+        final String goodVersion = kv[1];
+        if (unsupportedVersion.isEmpty() || goodVersion.isEmpty()) {
+            throw new CheckStylePluginException("Internal error: Property '" + PROP_VERSION_MAP + "' contains "
+                    + "invalid mapping '" + pMapping + "' in configuration file '" + pPropertyFile + "'");
+        }
+
+        if (!pSupportedVersions.contains(goodVersion)) {
+            throw new CheckStylePluginException("Internal error: Property '" + PROP_VERSION_MAP + "' contains "
+                    + "invalid mapping '" + pMapping + "'. Target version " + goodVersion + " is not a supported "
+                    + "version in configuration file '" + pPropertyFile + "'");
+        }
+        if (pSupportedVersions.contains(unsupportedVersion)) {
+            throw new CheckStylePluginException("Internal error: Property '" + PROP_VERSION_MAP + "' contains "
+                    + "invalid mapping '" + pMapping + "'. Checkstyle version " + unsupportedVersion + " is in "
+                    + "fact supported in configuration file '" + pPropertyFile + "'");
+        }
+        return new Pair<>(unsupportedVersion, goodVersion);
+    }
+
+
+    @NotNull
     public SortedSet<String> getSupportedVersions() {
         return supportedVersions;
     }
@@ -119,5 +188,10 @@ class VersionListReader
     @NotNull
     public static String getDefaultVersion(@NotNull final SortedSet<String> pSupportedVersions) {
         return pSupportedVersions.last();
+    }
+
+    @NotNull
+    public SortedMap<String, String> getReplacementMap() {
+        return replacementMap;
     }
 }
