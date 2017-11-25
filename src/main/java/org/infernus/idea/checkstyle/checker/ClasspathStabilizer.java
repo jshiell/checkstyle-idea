3595d260d459e4785b8b861b08a6be758a97feb9
Thomas Jensen <tsjensen@users.noreply.github.com>
2017-11-25 14:11:22 +0100

    Add ClasspathStabilizer to prevent libraries from getting locked by the "loader of checked code"

--- src/main/java/org/infernus/idea/checkstyle/checker/ClasspathStabilizer.java
+++ src/main/java/org/infernus/idea/checkstyle/checker/ClasspathStabilizer.java
@@ -1,8 +1,24 @@
 package org.infernus.idea.checkstyle.checker;
 
-import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.infernus.idea.checkstyle.util.TempDirProvider;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -12,16 +28,140 @@ import org.jetbrains.annotations.NotNull;
  */
 public class ClasspathStabilizer
 {
-    private final File tempDir;
+    private static final Logger LOG = Logger.getInstance(ClasspathStabilizer.class);
 
-    public ClasspathStabilizer(@NotNull final File pTempDir) {
-        tempDir = pTempDir;
+    static final String HASHFOLDER = "hashed";
+
+    /**
+     * number of characters in the relative path of the library up to which we would still use the path as-is; above
+     * that, we use hashes of the path instead in order to avoid path length problems on Windows
+     */
+    private static final int CLEAR_PATH_THRESHOLD_CHARS = 50;
+
+    private final Project project;
+
+    private final Path copyDir;
+
+
+    /**
+     * Constructor.
+     *
+     * @param pProject the current IDEA project
+     * @param pTempDir project-specific directory to keep copied libs in, retained after IDEA is closed
+     */
+    public ClasspathStabilizer(@NotNull final Project pProject, @NotNull final Path pTempDir) {
+        project = pProject;
+        copyDir = pTempDir;
     }
 
-    // TODO
 
-    public URL[] asArray() {
-        // TODO
-        return null;
+    @NotNull
+    public URL[] stabilize(@NotNull final List<URL> pUrls) {
+        URL[] result = null;
+        try {
+            final Optional<Path> projectDir = getProjectDir();
+            if (projectDir.isPresent()) {
+                final List<URL> stabilizedList = new ArrayList<>(pUrls.size() + 1);
+                for (final URL url : pUrls) {
+                    URL stabilizedUrl = url;
+                    if (residesInProject(projectDir.get(), url)) {
+                        stabilizedUrl = locateOrCreateCopy(projectDir.get(), url);
+                    }
+                    stabilizedList.add(stabilizedUrl);
+                }
+                result = stabilizedList.toArray(new URL[stabilizedList.size()]);
+            }
+        } catch (IOException | URISyntaxException | RuntimeException e) {
+            LOG.warn("Failed to stabilize the classpath. Using original classpath. Some files may become locked.", e);
+        }
+        if (result == null) {
+            result = pUrls.toArray(new URL[pUrls.size()]);
+        }
+        return result;
+    }
+
+
+    @NotNull
+    private URL locateOrCreateCopy(@NotNull final Path pProjectDir, final URL pUrl) throws IOException,
+            URISyntaxException {
+        final Path urlPath = Paths.get(pUrl.toURI()).toRealPath();
+        final Path relativePathSrc = pProjectDir.relativize(urlPath);
+        final Path relativePathTarget = buildTargetRelativePath(relativePathSrc);
+        final Path targetPath = copyDir.resolve(relativePathTarget);
+        if (needsUpdate(targetPath, urlPath)) {
+            if (targetPath.getParent() != null) {
+                Files.createDirectories(targetPath.getParent());
+            }
+            Files.copy(urlPath, targetPath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+        }
+        URL result = targetPath.toUri().toURL();
+        return result;
+    }
+
+
+    private boolean needsUpdate(@NotNull final Path pTargetPath, @NotNull final Path pSourcePath) throws IOException {
+        boolean result = true;
+        if (Files.exists(pTargetPath)) {
+            final long sizeSource = Files.size(pSourcePath);
+            final long sizeTarget = Files.size(pTargetPath);
+            final long modTimeSecsSource = Files.getLastModifiedTime(pSourcePath).to(TimeUnit.SECONDS);
+            final long modTimeSecsTarget = Files.getLastModifiedTime(pTargetPath).to(TimeUnit.SECONDS);
+            result = sizeSource != sizeTarget || modTimeSecsSource != modTimeSecsTarget;
+        }
+        return result;
+    }
+
+
+    @NotNull
+    private Path buildTargetRelativePath(@NotNull final Path pRelativePathSrc) {
+        Path result = pRelativePathSrc;
+        if (pRelativePathSrc.toString().length() > CLEAR_PATH_THRESHOLD_CHARS
+                || pRelativePathSrc.startsWith(HASHFOLDER)
+                || pRelativePathSrc.equals(Paths.get(TempDirProvider.README_FILE))) {
+            final Path parent = pRelativePathSrc.getParent();
+            final String parentHash = hash(parent != null ? parent.toString() : "");
+            result = Paths.get(HASHFOLDER, parentHash, pRelativePathSrc.getFileName().toString());
+        }
+        return result;
+    }
+
+    @NotNull
+    private String hash(@NotNull final String pString) {
+        final String s = pString.replaceAll(Pattern.quote("\\"), "/");
+        final String h = new Base32().encodeAsString(DigestUtils.sha1(s)).toLowerCase(Locale.ENGLISH);
+        final StringBuilder sb = new StringBuilder(h);
+        for (int i = sb.length() - 1; i >= 0 && sb.charAt(i) == '='; i--) {
+            sb.deleteCharAt(i);
+        }
+        return sb.toString();
+    }
+
+
+    private boolean residesInProject(@NotNull final Path pProjectDir, @NotNull final URL pUrl) throws IOException,
+            URISyntaxException {
+        boolean result = false;
+        if ("file".equals(pUrl.getProtocol())) {
+            Path urlPath = Paths.get(pUrl.toURI());
+            if (Files.isRegularFile(urlPath)) {
+                urlPath = urlPath.toRealPath();
+                result = urlPath.startsWith(pProjectDir);
+            }
+        }
+        return result;
+    }
+
+
+    private Optional<Path> getProjectDir() throws IOException {
+        Optional<Path> result = Optional.empty();
+        String basePath = project.getBasePath();
+        if (basePath != null) {
+            if (basePath.length() > 2 && (basePath.startsWith("/") || basePath.startsWith("\\")) && basePath.charAt
+                    (2) == ':') {
+                // e.g. "/D:/projects/foo", then we must cut off the leading slash
+                basePath = basePath.substring(1);
+            }
+            result = Optional.of(Paths.get(basePath).toRealPath());
+        }
+        return result;
     }
 }
---
+++ src/test/java/org/infernus/idea/checkstyle/checker/ClasspathStabilizerTest.java
@@ -0,0 +1,102 @@
+package org.infernus.idea.checkstyle.checker;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.intellij.openapi.project.Project;
+import org.hamcrest.core.IsEqual;
+import org.infernus.idea.checkstyle.util.TempDirProvider;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.Mockito;
+
+
+/**
+ * Unit tests of {@link ClasspathStabilizer}.
+ */
+public class ClasspathStabilizerTest
+{
+    private static final Project PROJECT = Mockito.mock(Project.class);
+
+    @Rule
+    public TemporaryFolder targetFolder = new TemporaryFolder();
+
+
+    @BeforeClass
+    public static void setup() {
+        String baseDir = ClasspathStabilizerTest.class.getResource("/cpstab").getPath();
+        Mockito.when(PROJECT.getBasePath()).thenReturn(baseDir);
+    }
+
+
+    private List<URL> buildInputClasspath() throws MalformedURLException {
+        final String baseDir = PROJECT.getBasePath();
+        List<URL> result = new ArrayList<>();
+        result.add(new File(baseDir, "lib1.jar").toURI().toURL());
+        result.add(new File(baseDir, "nonexistent.jar").toURI().toURL());
+        result.add(new File(baseDir, "libs/lib1.jar").toURI().toURL());
+        result.add(new File(baseDir, "libs/lib2.jar").toURI().toURL());
+        result.add(new File(baseDir, "hashed/lib1.jar").toURI().toURL());
+        result.add(new File(baseDir, "a_long_folder/path_more_than_50_characters/in_total/lib1.jar").toURI().toURL());
+        // SHA1("hashed") == "mmmfkoez3kxcsqlrrqbfbcxo5e4k6gq4"
+        result.add(new File(baseDir, "mmmfkoez3kxcsqlrrqbfbcxo5e4k6gq4/lib1.jar").toURI().toURL());
+        result.add(new File(baseDir, TempDirProvider.README_FILE).toURI().toURL());
+        return result;
+    }
+
+
+    @Test
+    public void testStabilizer() throws IOException {
+        final Path tempTargetDir = Paths.get(targetFolder.getRoot().toURI());
+        final List<URL> inputClasspath = buildInputClasspath();
+        final ClasspathStabilizer underTest = new ClasspathStabilizer(PROJECT, tempTargetDir);
+
+        final URL[] result = underTest.stabilize(inputClasspath);
+
+        final String baseDir = PROJECT.getBasePath();
+        final URL[] expected = new URL[]{
+                new File(targetFolder.getRoot(), "lib1.jar").toURI().toURL(),
+                new File(baseDir, "nonexistent.jar").toURI().toURL(),
+                new File(targetFolder.getRoot(), "libs/lib1.jar").toURI().toURL(),
+                new File(targetFolder.getRoot(), "libs/lib2.jar").toURI().toURL(),
+                new File(targetFolder.getRoot(), ClasspathStabilizer.HASHFOLDER +
+                        "/mmmfkoez3kxcsqlrrqbfbcxo5e4k6gq4/lib1.jar").toURI().toURL(),
+                new File(targetFolder.getRoot(), ClasspathStabilizer.HASHFOLDER +
+                        "/n6yrt7uzrjravbwjwxa2ed62cpl57vh5/lib1.jar").toURI().toURL(),
+                new File(targetFolder.getRoot(), "mmmfkoez3kxcsqlrrqbfbcxo5e4k6gq4/lib1.jar").toURI().toURL(),
+                new File(targetFolder.getRoot(), ClasspathStabilizer.HASHFOLDER +
+                        "/3i42h3s6nnfq2msvx7xzkyayscx5qbyj/" + TempDirProvider.README_FILE).toURI().toURL()
+        };
+        Assert.assertThat(expected, IsEqual.equalTo(result));
+    }
+
+
+    @Test
+    public void testUpdate() throws IOException, InterruptedException {
+        final String baseDir = PROJECT.getBasePath();
+        final Path tempTargetDir = Paths.get(targetFolder.getRoot().toURI());
+        final List<URL> inputClasspath = new ArrayList<>();
+        inputClasspath.add(new File(baseDir, "lib1.jar").toURI().toURL());
+        final ClasspathStabilizer underTest = new ClasspathStabilizer(PROJECT, tempTargetDir);
+
+        URL[] result = underTest.stabilize(inputClasspath);
+        final URL[] expected = new URL[]{new File(targetFolder.getRoot(), "lib1.jar").toURI().toURL()};
+        Assert.assertTrue(Arrays.equals(expected, result));
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+
+        result = underTest.stabilize(inputClasspath);
+        Assert.assertTrue(Arrays.equals(expected, result));
+    }
+}
