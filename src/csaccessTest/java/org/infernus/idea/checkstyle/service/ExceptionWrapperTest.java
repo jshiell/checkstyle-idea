c042f15241961d5535904f9fdfab390e81760e65
Jamie Shiell <jamie@infernus.org>
2018-10-07 15:20:29 +0100

    Added test for StringIndexOutOfBounds on exception wrapper (#413)

--- src/csaccessTest/java/org/infernus/idea/checkstyle/service/ExceptionWrapperTest.java
+++ src/csaccessTest/java/org/infernus/idea/checkstyle/service/ExceptionWrapperTest.java
@@ -7,73 +7,81 @@ import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import org.infernus.idea.checkstyle.exception.CheckStylePluginException;
 import org.infernus.idea.checkstyle.exception.CheckStylePluginParseException;
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 
-public class ExceptionWrapperTest
-{
+public class ExceptionWrapperTest {
     @Test
     public void anExceptionThatIsNotACheckstyleExceptionIsWrappedInAPluginException() {
-        assertThat( //
-                new ExceptionWrapper().wrap(null, new NullPointerException("aTestException")), //
+        assertThat(
+                new ExceptionWrapper().wrap(null, new NullPointerException("aTestException")),
                 is(instanceOf(CheckStylePluginException.class)));
     }
 
     @Test
     public void aCheckstyleExceptionThatHasACauseOfARecognitionExceptionIsWrappedInAPluginParseException() {
-        assertThat( //
-                new ExceptionWrapper().wrap(null, new CheckstyleException("aTestException", new RecognitionException
-                        ("aTestException"))), //
+        assertThat(
+                new ExceptionWrapper()
+                        .wrap(null, new CheckstyleException("aTestException", new RecognitionException("aTestException"))),
                 is(instanceOf(CheckStylePluginParseException.class)));
     }
 
     @Test
     public void aCheckstyleExceptionThatHasACauseOfASubclassOfRecognitionExceptionIsWrappedInAPluginParseException() {
-        assertThat( //
-                new ExceptionWrapper().wrap(null, new CheckstyleException("aTestException", new
-                        MismatchedTokenException())), //
+        assertThat(
+                new ExceptionWrapper()
+                        .wrap(null, new CheckstyleException("aTestException", new MismatchedTokenException())),
                 is(instanceOf(CheckStylePluginParseException.class)));
     }
 
     @Test
     public void aCheckstyleExceptionThatHasACauseOfATokenStreamExceptionIsWrappedInAPluginParseException() {
-        assertThat( //
-                new ExceptionWrapper().wrap(null, new CheckstyleException("aTestException", new TokenStreamException
-                        ("aTestException"))), //
+        assertThat(
+                new ExceptionWrapper()
+                        .wrap(null, new CheckstyleException("aTestException", new TokenStreamException("aTestException"))),
                 is(instanceOf(CheckStylePluginParseException.class)));
     }
 
     @Test
     public void aCheckstyleExceptionThatHasACauseOfANullPointerExceptionIsWrappedInAPluginParseException() {
-        assertThat( //
-                new ExceptionWrapper().wrap(null, new CheckstyleException("aTestException", new NullPointerException
-                        ("aTestException"))), //
+        assertThat(
+                new ExceptionWrapper()
+                        .wrap(null, new CheckstyleException("aTestException", new NullPointerException("aTestException"))),
                 is(instanceOf(CheckStylePluginParseException.class)));
     }
 
     @Test
     public void aCheckstyleExceptionThatHasACauseOfAnArrayIndexOutOfBoundsExceptionIsWrappedInAPluginParseException() {
-        assertThat( //
-                new ExceptionWrapper().wrap(null, new CheckstyleException("aTestException", new
-                        ArrayIndexOutOfBoundsException("aTestException"))), //
+        assertThat(
+                new ExceptionWrapper()
+                        .wrap(null, new CheckstyleException("aTestException", new ArrayIndexOutOfBoundsException("aTestException"))),
                 is(instanceOf(CheckStylePluginParseException.class)));
     }
 
     @Test
     public void aCheckstyleExceptionThatHasACauseOfAnIllegalStateExceptionIsWrappedInAPluginParseException() {
-        assertThat( //
-                new ExceptionWrapper().wrap(null, new CheckstyleException("aTestException", new IllegalStateException
-                        ("aTestException"))), //
+        assertThat(
+                new ExceptionWrapper()
+                        .wrap(null, new CheckstyleException("aTestException", new IllegalStateException("aTestException"))),
+                is(instanceOf(CheckStylePluginParseException.class)));
+    }
+
+    @Test
+    public void aCheckstyleExceptionThatHasACauseOfAStringIndexOutOfBoundsExceptionIsWrappedInAPluginParseException() {
+        assertThat(
+                new ExceptionWrapper()
+                        .wrap(null, new CheckstyleException("aTestException", new StringIndexOutOfBoundsException("aTestException"))),
                 is(instanceOf(CheckStylePluginParseException.class)));
     }
 
     @Test
     public void aCheckstyleExceptionThatHasACauseOfAnyOtherExceptionIsWrappedInAPluginException() {
-        assertThat( //
-                new ExceptionWrapper().wrap(null, new CheckstyleException("aTestException", new IllegalStateException
-                        ("aTestException"))), //
+        assertThat(
+                new ExceptionWrapper()
+                        .wrap(null, new CheckstyleException("aTestException", new IllegalStateException("aTestException"))),
                 is(instanceOf(CheckStylePluginException.class)));
     }
 }
