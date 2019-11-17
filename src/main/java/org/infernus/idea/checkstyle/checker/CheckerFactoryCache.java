e458b745cd9b6c3683b313dcabdb0d79ccbc3850
Jamie Shiell <jamie@infernus.org>
2019-11-17 11:40:34 +0000

    Simplified the checker factory cache - locking isn't as fine, but it's a lot more readable. Checkers should also now be destroyed only after they've been removed from the cache.

--- src/main/java/org/infernus/idea/checkstyle/checker/CheckerFactoryCache.java
+++ src/main/java/org/infernus/idea/checkstyle/checker/CheckerFactoryCache.java
@@ -1,85 +1,66 @@
 package org.infernus.idea.checkstyle.checker;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
 import com.intellij.concurrency.JobScheduler;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import org.infernus.idea.checkstyle.model.ConfigurationLocation;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class CheckerFactoryCache {
+
+    private static final Logger LOG = Logger.getInstance(CheckerFactoryCache.class);
 
     private static final int CLEANUP_PERIOD_SECONDS = 30;
 
-    // TODO This may work more reliably if we just used a ConcurrentHashMap instead of our own reimplementation.
-    // TODO We should check for expiration only when we return a Checker from the cache, so that we don't need the
-    //      backgroundCleanupTask.
-    private final ReadWriteLock cacheLock = new ReentrantReadWriteLock();
-    private final Map<CheckerFactoryCacheKey, CachedChecker> cache = new HashMap<>();
-
+    private final Map<CheckerFactoryCacheKey, CachedChecker> cache = new ConcurrentHashMap<>();
     private final ScheduledExecutorService cleanUpExecutor = JobScheduler.getScheduler();
 
     public CheckerFactoryCache() {
         startBackgroundCleanupTask();
     }
 
-    public Optional<CachedChecker> get(@NotNull final ConfigurationLocation location, @Nullable final Module module) {
+    public Optional<CachedChecker> get(@NotNull final ConfigurationLocation location,
+                                       @Nullable final Module module) {
         final CheckerFactoryCacheKey key = new CheckerFactoryCacheKey(location, module);
-        cacheLock.readLock().lock();
-        try {
-            if (cache.containsKey(key)) {
-                final CachedChecker cachedChecker = cache.get(key);
-                if (cachedChecker != null && cachedChecker.isValid()) {
-                    return Optional.of(cachedChecker);
-                } else {
-                    cacheLock.readLock().unlock();
-                    writeToCache(() -> {
-                        try {
-                            if (cachedChecker != null) {
-                                cachedChecker.destroy();
-                            }
-                        } catch (Exception ignored) {
-                        }
-                        return cache.remove(key);
-                    });
-                    cacheLock.readLock().lock();
-                }
-            }
-            return Optional.empty();
-        } finally {
-            cacheLock.readLock().unlock();
+
+        final CachedChecker cachedChecker = cache.get(key);
+        if (cachedChecker != null && cachedChecker.isValid()) {
+            return Optional.of(cachedChecker);
         }
+
+        if (cachedChecker != null) {
+            cache.remove(key);
+            destroyChecker(cachedChecker);
+        }
+        return Optional.empty();
     }
 
     public void put(@NotNull final ConfigurationLocation location,
-                    final Module module,
+                    @Nullable final Module module,
                     @NotNull final CachedChecker checker) {
-        writeToCache(() -> cache.put(new CheckerFactoryCacheKey(location, module), checker));
+        cache.put(new CheckerFactoryCacheKey(location, module), checker);
     }
 
     public void invalidate() {
-        writeToCache(() -> {
-            try {
-                cache.values().forEach(this::destroyChecker);
-            } finally {
-                cache.clear();
-            }
-            return null;
-        });
+        List<CachedChecker> existingCheckers = new ArrayList<>(cache.values());
+        cache.clear();
+
+        existingCheckers.forEach(this::destroyChecker);
     }
 
-    private void destroyChecker(@NotNull final CachedChecker pCachedChecker) {
-        pCachedChecker.destroy();
+    private void destroyChecker(final CachedChecker cachedChecker) {
+        try {
+            if (cachedChecker != null) {
+                cachedChecker.destroy();
+            }
+        } catch (Exception ignored) {
+        }
     }
 
     private void startBackgroundCleanupTask() {
@@ -91,22 +72,21 @@ public class CheckerFactoryCache {
     }
 
     private void cleanUpExpiredCachedCheckers() {
-        writeToCache(() -> {
-            final List<CheckerFactoryCacheKey> itemsToRemove = cache.entrySet().stream().filter(cacheEntry ->
-                    cacheEntry.getValue() != null && !cacheEntry.getValue().isValid()).map(cacheEntry -> {
-                destroyChecker(cacheEntry.getValue());
-                return cacheEntry.getKey();
-            }).collect(Collectors.toList());
-            return cache.entrySet().removeIf(entry -> itemsToRemove.contains(entry.getKey()));
-        });
-    }
-
-    private <T> T writeToCache(final Supplier<T> task) {
-        cacheLock.writeLock().lock();
         try {
-            return task.get();
-        } finally {
-            cacheLock.writeLock().unlock();
+            List<CachedChecker> checkersToDestroy = new ArrayList<>();
+
+            for (Iterator<Map.Entry<CheckerFactoryCacheKey, CachedChecker>> i = cache.entrySet().iterator(); i.hasNext();) {
+                Map.Entry<CheckerFactoryCacheKey, CachedChecker> cacheEntry = i.next();
+                if (cacheEntry.getValue() == null || !cacheEntry.getValue().isValid()) {
+                    checkersToDestroy.add(cacheEntry.getValue());
+                    i.remove();
+                }
+            }
+
+            checkersToDestroy.forEach(this::destroyChecker);
+
+        } catch (Exception e) {
+            LOG.error("Cleanup failed", e);
         }
     }
 }
