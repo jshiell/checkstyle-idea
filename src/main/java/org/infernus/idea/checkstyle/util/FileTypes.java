214f46fc82cc68df8288a7db0446c45e20c788fc
Jamie Shiell <jamie@infernus.org>
2021-04-06 14:42:07 +0100

    Fixed deprecated file type for 2020.2 SDK

--- src/main/java/org/infernus/idea/checkstyle/util/FileTypes.java
+++ src/main/java/org/infernus/idea/checkstyle/util/FileTypes.java
@@ -1,7 +1,7 @@
 package org.infernus.idea.checkstyle.util;
 
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.StdFileTypes;
 
 public final class FileTypes {
 
@@ -9,7 +9,7 @@ public final class FileTypes {
     }
 
     public static boolean isJava(final FileType fileType) {
-        return fileType != null && StdFileTypes.JAVA.equals(fileType);
+        return JavaFileType.INSTANCE.equals(fileType);
     }
 
 }
