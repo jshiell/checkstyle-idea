81111e4c306b77082a17e076820cceb211cb2aec
AkMo3 <akmo3901@gmail.com>
2021-04-20 20:27:35 +0530

    Issue #522: LocalizedMessage Class renamed to Violation in checkstyle

--- src/csaccessTest/java/org/infernus/idea/checkstyle/service/CheckStyleAuditListenerTest.java
+++ src/csaccessTest/java/org/infernus/idea/checkstyle/service/CheckStyleAuditListenerTest.java
@@ -1,12 +1,12 @@
 package org.infernus.idea.checkstyle.service;
 
 import com.puppycrawl.tools.checkstyle.api.AuditEvent;
-import com.puppycrawl.tools.checkstyle.api.LocalizedMessage;
 import com.puppycrawl.tools.checkstyle.api.SeverityLevel;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
 import java.util.Collections;
 import java.util.Optional;
 
@@ -15,7 +15,7 @@ public class CheckStyleAuditListenerTest {
     private int counter = 0;
 
     @Test
-    public void testSeverityLevels() {
+    public void testSeverityLevels() throws Exception {
         final CheckStyleAuditListener underTest = new CheckStyleAuditListener(Collections.emptyMap(), false, 2,
                 Optional.empty(), Collections.emptyList());
         underTest.addError(createDummyEvent(SeverityLevel.INFO));
@@ -26,7 +26,7 @@ public class CheckStyleAuditListenerTest {
     }
 
     @Test
-    public void testAddException() {
+    public void testAddException() throws Exception {
         final CheckStyleAuditListener underTest = new CheckStyleAuditListener(Collections.emptyMap(), false, 2,
                 Optional.empty(), Collections.emptyList());
         underTest.addException(createDummyEvent(SeverityLevel.ERROR), //
@@ -45,9 +45,51 @@ public class CheckStyleAuditListenerTest {
         }
     }
 
-    private AuditEvent createDummyEvent(@Nullable final SeverityLevel pSeverityLevel) {
-        LocalizedMessage localizedMessage = new LocalizedMessage(42 + (counter++), 21, "bundle", "message text",
+    private AuditEvent createDummyEvent(@Nullable final SeverityLevel pSeverityLevel) throws Exception {
+        Constructor<?> auditEvent = getAuditEvent();
+        Class<?> messageClass = getMessageClass();
+        Class<?>[] Type = {int.class, int.class, String.class, String.class, Object[].class,
+                SeverityLevel.class, String.class, Class.class, String.class};
+        Constructor<?> cons = messageClass.getDeclaredConstructor(Type);
+
+        Object message = cons.newInstance(42 + (counter++), 21, "bundle", "message text",
                 null, pSeverityLevel, "moduleId", CheckStyleAuditListenerTest.class, null);
-        return new AuditEvent("source", "filename.java", localizedMessage);
+        return (AuditEvent) auditEvent.newInstance("source", "filename.java", message);
+    }
+
+    private Class<?> getMessageClass() throws NullPointerException, ClassNotFoundException {
+        Class messageClass = null;
+        try {
+            messageClass = Class.forName("com.puppycrawl.tools.checkstyle.api.Violation");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Violation class not found");
+        }
+
+        if (messageClass == null) {
+            try {
+                messageClass = Class.forName("com.puppycrawl.tools.checkstyle.api.LocalizedMessage");
+            } catch (ClassNotFoundException e) {
+                System.out.println("LocalizedClass class Not found");
+                e.printStackTrace();
+                throw e;
+            }
+        }
+        return messageClass;
+    }
+
+    private Constructor<?> getAuditEvent() throws Exception {
+        final Class<?> auditEventClass;
+        final Constructor<?> auditEvent;
+        try {
+            auditEventClass = Class.forName("com.puppycrawl.tools.checkstyle.api.AuditEvent");
+        } catch (ClassNotFoundException e) {
+            System.out.println("AuditEvent class Not found");
+            e.printStackTrace();
+            throw e;
+        }
+
+        Class<?>[] type = { Object.class, String.class, getMessageClass()};
+        auditEvent = auditEventClass.getDeclaredConstructor(type);
+        return auditEvent;
     }
 }
