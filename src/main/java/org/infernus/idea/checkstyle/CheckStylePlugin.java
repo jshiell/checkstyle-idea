289811b5a2f8856e5ae93dc4b2054b7cf67ed925
Jamie Shiell <jamie@infernus.org>
2026-05-30 17:14:04 +0100

    Remove deprecated (2026.2) and seemingly unused method

--- src/main/java/org/infernus/idea/checkstyle/CheckStylePlugin.java
+++ src/main/java/org/infernus/idea/checkstyle/CheckStylePlugin.java
@@ -1,9 +1,5 @@
 package org.infernus.idea.checkstyle;
 
-import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.ide.plugins.PluginManagerCore;
-import com.intellij.openapi.extensions.PluginId;
-
 public final class CheckStylePlugin {
 
     private CheckStylePlugin() {
@@ -13,17 +9,5 @@ public final class CheckStylePlugin {
      * The plugin ID. Caution: It must be identical to the String set in build.gradle at intellij.pluginName
      */
     public static final String ID_PLUGIN = "CheckStyle-IDEA";
-
-    public static String version() {
-        try {
-            final IdeaPluginDescriptor plugin = PluginManagerCore.getPlugin(PluginId.getId(ID_PLUGIN));
-            if (plugin != null) {
-                return plugin.getVersion();
-            }
-        } catch (Throwable ignored) {
-            // thrown if plugin isn't initialised, e.g. during tests
-        }
-        return "unknown";
-    }
 
 }
