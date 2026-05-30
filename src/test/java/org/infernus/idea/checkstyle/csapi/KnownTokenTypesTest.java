5c9a6ec924a4186ff5a960f79dd11552b5bb47f2
Jamie Shiell <jamie@infernus.org>
2026-05-30 13:45:06 +0100

    test: add KnownTokenTypesTest for token type enum

---
+++ src/test/java/org/infernus/idea/checkstyle/csapi/KnownTokenTypesTest.java
@@ -0,0 +1,31 @@
+package org.infernus.idea.checkstyle.csapi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+class KnownTokenTypesTest {
+
+    @Test
+    void allExpectedTokensAreDeclared() {
+        KnownTokenTypes[] values = KnownTokenTypes.values();
+        assertThat(values.length, is(57));
+    }
+
+    @Test
+    void assignTokenIsPresent() {
+        assertThat(KnownTokenTypes.valueOf("ASSIGN"), is(KnownTokenTypes.ASSIGN));
+    }
+
+    @Test
+    void classDefTokenIsPresent() {
+        assertThat(KnownTokenTypes.valueOf("CLASS_DEF"), is(KnownTokenTypes.CLASS_DEF));
+    }
+
+    @Test
+    void methodDefTokenIsPresent() {
+        assertThat(KnownTokenTypes.valueOf("METHOD_DEF"), is(notNullValue()));
+    }
+}
